fe47e7cc7bd899dbecb8d7175d826af6b8f6ccfa
Herbalice <30403716+Herbalice@users.noreply.github.com>
2017-07-24 17:33:16 +0800

    skill已經更新

---
+++ js/custom-scripts.js
@@ -0,0 +1,37 @@
+(function($) {
+    
+  'use strict';
+
+
+    /**
+     * =======================================
+     * Top Navigaion Init
+     * =======================================
+     */
+    var navigation = $('#js-navbar-menu').okayNav({
+      toggle_icon_class: "okayNav__menu-toggle",
+      toggle_icon_content: "<span /><span /><span /><span /><span />"
+    });
+
+
+
+    /**
+     * =======================================
+     * Top Fixed Navbar
+     * =======================================
+     */
+    $(document).on('scroll', function() {
+      var activeClass = 'navbar-home',
+          ActiveID        = '.main-navbar-top',
+          scrollPos       = $(this).scrollTop();
+
+      if( scrollPos > 10 ) {
+        $( ActiveID ).addClass( activeClass );
+      } else {
+        $( ActiveID ).removeClass( activeClass );
+      }
+    });    
+
+
+} (jQuery) );
+
